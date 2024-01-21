2dcaa9035597adbec6e528a9ea5e343c479911cb
sujan khatri <sujansince2059@gmail.com>
2024-01-21 15:38:08 +0545

    for making import export easyy

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import About from "./Aboutus";
+import Contact from "./Contactus";
+import Hello from "./Hello";
+import Blog from "./Blog";
+import Github from "./Github"
+
+
+
+export {Header,Footer,Home,About,Contact,Hello,Blog,Github}
