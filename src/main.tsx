26e8b6c3673919338952c5379546dd689efaee9a
sujan khatri <sujansince2059@gmail.com>
2024-01-21 15:37:13 +0545

    defined routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout";
+import { About, Blog, Contact, Github, Home } from "./components";
+
+// defining the router props for RouterProvider
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "", // / paxi kei na vaye render Home
+        element: <Home />,
+      },
+      {
+        path: "about", // / ta pailai xa as hamle  / ko children ma yo lekhdai xam. means /about ma k dekhauni
+        element: <About />,
+      },
+      {
+        path: "contactus", // / ta pailai xa as hamle  / ko children ma yo lekhdai xam. means /about ma k dekhauni
+        element: <Contact />,
+      },
+      {
+        path: "blogid/:userid", // / ta pailai xa as hamle  / ko children ma yo lekhdai xam. means /about ma k dekhauni
+        element: <Blog />,
+      },
+      {
+        path: "github", // / ta pailai xa as hamle  / ko children ma yo lekhdai xam. means /about ma k dekhauni
+        element: <Github />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
