873adc8e5fce68fb7b3e654b20c4b0de108ff424
sujan khatri <sujansince2059@gmail.com>
2024-01-22 09:49:43 +0545

    mis.c

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,10 +3,9 @@ import Footer from "./Footer";
 import Home from "./Home";
 import About from "./Aboutus";
 import Contact from "./Contactus";
-import Hello from "./Hello";
 import Blog from "./Blog";
 import Github from "./Github"
 
 
 
-export {Header,Footer,Home,About,Contact,Hello,Blog,Github}
+export {Header,Footer,Home,About,Contact,Blog,Github}
